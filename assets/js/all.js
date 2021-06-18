e3709774d210ea65045ed19c5a307a2d0784aa41
Romain GROULT <groult.romain92@gmail.com>
2021-06-18 15:50:01 +0200

    1st row finished

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,9 @@
-$(window).scroll(function(){
-    if ($(this).scrollTop() > 50) {
-       $('slide').addClass('slide-active');
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 500) {
+      $("slide-2").addClass("slide-active");
     } else {
-       $('slide').removeClass('slide-active');
+      $("slide-2").removeClass("slide-active");
     }
+  });
 });
