8a6c6f77559298a43b36ad06c2124aa3c0e090a9
Romain GROULT <groult.romain92@gmail.com>
2021-06-18 17:19:53 +0200

    Rows 2 & 3 ok

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $(window).scroll(function() {
-    if ($(document).scrollTop() > 500) {
-      $("slide-2").addClass("slide-active");
+    if ($(document).scrollTop() > 100) {
+      $("#slider").addClass("slide-scrolled");
     } else {
-      $("slide-2").removeClass("slide-active");
+      $("#slider").removeClass("slide-scrolled");
     }
   });
 });
